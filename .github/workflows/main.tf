5c722a622835602879e41cc694c7af38c4b0b5c5
njohdesign <46420553+njohdesign@users.noreply.github.com>
2026-09-11 09:49:31 +0100

    Add Terraform configuration for AWS S3 bucket
    This Terraform configuration sets up an AWS S3 bucket with a specified name and tags.

---
+++ .github/workflows/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example_bucket2" {
+  bucket = "unique-name-for-your-lab-bucket"  # Replace with a unique bucket name
+
+  acl = "private"
+
+  tags = {
+    Environment = "Lab"
+  }
+}
